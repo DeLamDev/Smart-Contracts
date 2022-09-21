cafd7571e8fe2400ab6f70f8a5321f8e05fbadce
Habib <delamadrid.dev@protonmail.com>
2022-09-21 15:13:16 -0500

    Actualización test

--- Base/tokens/ERC20/pruebas/test/bitcoin_replica.js
+++ Base/tokens/ERC20/pruebas/test/bitcoin_replica.js
@@ -1,4 +1,4 @@
-const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { loadFixture, time } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
 
 describe("Bitcoin Replica Contract", function () {
@@ -24,7 +24,7 @@ describe("Bitcoin Replica Contract", function () {
 		it("Should allow to mine the first block after deployment", async function() {
 			const { Bitcoin_Replica, owner } = await loadFixture(deployTokenFixture);
 
-			await expect(Bitcoin_Replica.mine(owner.address).to.changeTokenBalance(Bitcoin_Replica, owner, (50*10**18)));
+			await expect(Bitcoin_Replica.mine(owner.address)).to.changeTokenBalance(Bitcoin_Replica, owner, (50*10**18).toString());
 
 		});
 
@@ -34,7 +34,24 @@ describe("Bitcoin Replica Contract", function () {
 		it("Should be able to transfer mined tokens", async function() {
 			const { Bitcoin_Replica, owner, addr1 } = await loadFixture(deployTokenFixture);
 
-			await expect(Bitcoin_Replica.transfer(addr1.address, 50).to.changeTokenBalances(Bitcoin_Replica, [owner, addr1], [-50, 50]));
+			await expect(Bitcoin_Replica.mine(owner.address)).to.changeTokenBalance(Bitcoin_Replica, owner, (50*10**18).toString());
+			await expect(Bitcoin_Replica.transfer(addr1.address, (50*10**18).toString())).to.changeTokenBalances(Bitcoin_Replica, [owner, addr1], [(-(50*10**18)).toString(), (50*10**18).toString()]);
+
+		});
+		it("Should mine until the reward is equal to 0", async function() {
+			const { Bitcoin_Replica, owner } = await loadFixture(deployTokenFixture);
+			
+			const initial_time = await Bitcoin_Replica.genesis();
+			const ten_min = 600;
+			let counter = initial_time;
+			while (await Bitcoin_Replica.current_reward() != 0) {
+				await expect(Bitcoin_Replica.mine(owner.address));
+				counter = Number(counter) + ten_min;
+				await time.increaseTo(counter);
+				console.log(await Bitcoin_Replica.current_reward());
+				console.log(await Bitcoin_Replica.next_block());
+
+			};
 		});
 	});
 	
